30c01ce590d4f0d9e92450f68468ed13a57bdeb9
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-04-18 13:52:11 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Samuel Hernandez Vicera on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
